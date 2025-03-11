1b263fe33dc6cb8ebbb5aecb936e1feb5f4f25c4
Saittem <TEOLF@email.cz>
2025-03-11 22:44:14 +0100

    Game update
    Added enemy movement
    Added enemy_barrier for enemies to not fall into pits with spikes

--- platformer/scripts/enemy_wolf.gd
+++ platformer/scripts/enemy_wolf.gd
@@ -1,7 +1,26 @@
 class_name Enemy
 extends CharacterBody2D
 
-# Handles gravity
+@onready var target = $"../Player"
+@export var speed: float = 150.0
+@onready var animated_sprite = $"AnimatedSprite2D"
+
 func _physics_process(delta):
+	var direction = (target.position - position).normalized()
+	
+	# Apply horizontal movement
+	velocity.x = direction.x * speed
+
+	# Apply gravity if not on the floor
 	if not is_on_floor():
-		velocity = get_gravity() * delta
+		velocity.y += get_gravity().y * delta  
+
+	# Flip sprite based on movement direction
+	if direction.x > 0:
+		animated_sprite.flip_h = false
+	elif direction.x < 0:
+		animated_sprite.flip_h = true
+
+	move_and_slide()
+
+	print("Direction: ", direction)
--- platformer/scripts/main_menu.gd
+++ platformer/scripts/main_menu.gd
@@ -1,4 +1,9 @@
 extends Control
 
+@onready var playBtn = $buttons/playButton
+
+func _ready():
+	playBtn.add_theme_color_override("font_hover_color", Color("#000000"))
+
 func _on_play_pressed():
 	get_tree().change_scene_to_file("res://assets/scenes/game.tscn")
